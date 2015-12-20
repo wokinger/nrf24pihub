5fa1ee11e72c497303a00352e98b37283993b20e
wokinger <romanleitner@gmail.com>
2015-12-20 21:09:23 +0000

    first working, but not reliable

--- raspberryfriend.py
+++ raspberryfriend.py
@@ -7,18 +7,22 @@ from nrf24 import NRF24
 import time
 from time import gmtime, strftime
 
-pipes = [[0xf0, 0xf0, 0xf0, 0xf0, 0xe1], [0xf0, 0xf0, 0xf0, 0xf0, 0xd2]]
+pipes = [[0xDE, 0xAD, 0xBE, 0xEF, 0xff], [0xDE, 0xAD, 0xBE, 0xEF, 11]]
+#RX Address
+rx_pipe = [0xDE, 0xAD, 0xBE, 0xEF, 11]
+tx_pipe = [0xDE, 0xAD, 0xBE, 0xEF, 0xff]
 
 radio = NRF24()
 radio.begin(0, 0,25,18) #set gpio 25 as CE pin
 radio.setRetries(15,15)
 radio.setPayloadSize(32)
-radio.setChannel(0x4c)
-radio.setDataRate(NRF24.BR_250KBPS)
+radio.setChannel(0x00)
+radio.setCRCLength(NRF24.CRC_8)
+radio.setDataRate(NRF24.BR_1MBPS)
 radio.setPALevel(NRF24.PA_MAX)
 radio.setAutoAck(1)
-radio.openWritingPipe(pipes[0])
-radio.openReadingPipe(1, pipes[1])
+radio.openWritingPipe(tx_pipe)
+radio.openReadingPipe(1, rx_pipe)
 
 radio.startListening()
 radio.stopListening()
@@ -30,6 +34,8 @@ while True:
     pipe = [0]
     while not radio.available(pipe, True):
         time.sleep(1000/1000000.0)
+        #radio.printDetails()
+        
     recv_buffer = []
     radio.read(recv_buffer)
     out = ''.join(chr(i) for i in recv_buffer)
